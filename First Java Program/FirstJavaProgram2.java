ed778fcbb268a9d78a9c395a0ffb3395328da061
Ashley D'Allessandro <ashley.dallessandro@gmail.com>
2021-04-07 07:13:27 -0700

    Add a second one for practice purposes

---
+++ First Java Program/FirstJavaProgram2.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram2{
+    public static void main(String[] args){
+        System.out.println("Hello, my name is Inigo Montoya");
+        System.out.println("You killed my father");
+        System.out.println("Prepare to die");
+    }
+}
